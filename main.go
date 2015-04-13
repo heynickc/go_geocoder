682d6d3657d34713af5e23cd8cda672dbb353b76
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-04-13 19:56:23 -0400

    Separating out the mdimap struct

--- main.go
+++ main.go
@@ -14,9 +14,6 @@ func (a *Address) SingleLine() string {
 	return a.street + " " + a.city + "," + a.state + " " + a.zip
 }
 
-type MDiMapGeocoder struct {
-}
-
 func (m *MDiMapGeocoder) Geocode(url string) {
 	if _, err := http.Get(os.Stdout, url); err != nil {
 		log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
@@ -25,10 +22,8 @@ func (m *MDiMapGeocoder) Geocode(url string) {
 
 func main() {
 
-	var g = new(MDiMapGeocoder)
+	var g = MDiMapGeocoder.New()
 
-	var url = "http://geodata.md.gov/imap/rest/services/GeocodeServices/MD_CompositeLocatorWithZIPCodeCentroids/GeocodeServer/findAddressCandidates?Street=507+S+Pinehurst+Ave&City=Salisbury&State=Maryland&ZIP=21801&SingleLine=&outFields=&maxLocations=United+States&outSR=4326&searchExtent=&f=pjson"
-
-	g.Geocode(url)
+	g.Geocode(g.URL.String())
 
 }
