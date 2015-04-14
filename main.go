484047ac8cd7f69c21349f97f93e6695a9577234
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-04-13 21:18:01 -0400

    Custom type for geocoding

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gorilla/http"
 	"log"
+	"net/url"
 	"os"
 )
 
@@ -14,7 +15,36 @@ func (a *Address) SingleLine() string {
 	return a.street + " " + a.city + "," + a.state + " " + a.zip
 }
 
-func (m *MDiMapGeocoder) Geocode(url string) {
+type Geocoder struct {
+	httpUrl *url.URL
+}
+
+func New() *Geocoder {
+
+	u := new(url.URL)
+
+	u.Scheme = "http"
+	u.Host = "geodata.md.gov"
+	u.Path = "imap/rest/services/GeocodeServices/MD_CompositeLocatorWithZIPCodeCentroids/GeocodeServer/findAddressCandidates"
+
+	v := url.Values{
+		"Street":       []string{"507 S Pinehurst Ave"},
+		"City":         []string{"Salisbury"},
+		"State":        []string{"Maryland"},
+		"ZIP":          []string{"21801"},
+		"SingleLine":   []string{""},
+		"outFields":    []string{""},
+		"maxLocations": []string{"United States"},
+		"outSR":        []string{"4326"},
+		"searchExtent": []string{""},
+		"f":            []string{"json"},
+	}
+	u.RawQuery = v.Encode()
+
+	return &Geocoder{u}
+}
+
+func (m *Geocoder) Geocode(url string) {
 	if _, err := http.Get(os.Stdout, url); err != nil {
 		log.Fatalf("unable to fetch %q: %v", os.Args[1], err)
 	}
@@ -22,8 +52,7 @@ func (m *MDiMapGeocoder) Geocode(url string) {
 
 func main() {
 
-	var g = MDiMapGeocoder.New()
+	gc := New()
 
-	g.Geocode(g.URL.String())
-
+	gc.Geocode(gc.httpUrl.String())
 }
