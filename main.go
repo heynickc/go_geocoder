21715932cf3fe5ce24824ac0c5bf39a788bb1211
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-04-14 21:25:08 -0400

    Json marshalling

--- main.go
+++ main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"compress/gzip"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
 )
+
+type Address struct {
+	Address  string
+	Location Location
+	Score    float32
+}
+
+type Location struct {
+	X float32
+	Y float32
+}
+
+type AddressMarshaler interface {
+	MarshalAddresses(writer io.Writer, addresses []*Address) error
+}
+
+type AddressUnmarshaler interface {
+	UnmarshalAddresses(reader io.Reader) ([]*Address, error)
+}
+
+type JSONMarshaler struct{}
+
+func (JSONMarshaler) MarshalAddresses(writer io.Writer,
+	addresses []*Address) error {
+	encoder := json.NewEncoder(writer)
+	return encoder.Encode(addresses)
+}
+
+func (JSONMarshaler) UnmarshalAddresses(reader io.Reader) ([]*Address,
+	error) {
+	decoder := json.NewDecoder(reader)
+	var addresses []*Address
+	if err := decoder.Decode(&addresses); err != nil {
+		return nil, err
+	}
+	return addresses, nil
+}
 
 type Geocoder struct {
 	URL *url.URL
@@ -54,5 +99,115 @@ func (g *Geocoder) Geocode() []byte {
 }
 
 func main() {
+	addresses, err := readDataFile("./my_address.json")
+	if err != nil {
+		log.Fatalln("Failed to read:", err)
+	}
 
+	fmt.Println(addresses)
+}
+
+func readDataFile(filename string) ([]*Address, error) {
+	file, closer, err := openDataFile(filename)
+	if closer != nil {
+		defer closer()
+	}
+	if err != nil {
+		return nil, err
+	}
+	return readData(file, suffixOf(filename))
+}
+
+func openDataFile(filename string) (io.ReadCloser, func(), error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	closer := func() { file.Close() }
+	var reader io.ReadCloser = file
+	var decompressor *gzip.Reader
+	if strings.HasSuffix(filename, ".gz") {
+		if decompressor, err = gzip.NewReader(file); err != nil {
+			return file, closer, err
+		}
+		closer = func() { decompressor.Close(); file.Close() }
+		reader = decompressor
+	}
+	return reader, closer, nil
+}
+
+func readData(reader io.Reader, suffix string) ([]*Address, error) {
+	var unmarshaler AddressUnmarshaler
+	switch suffix {
+	// case ".gob":
+	// 	unmarshaler = GobMarshaler{}
+	// case ".inv":
+	// 	unmarshaler = InvMarshaler{}
+	case ".jsn", ".json":
+		unmarshaler = JSONMarshaler{}
+		// case ".txt":
+		// 	unmarshaler = TxtMarshaler{}
+		// case ".xml":
+		// 	unmarshaler = XMLMarshaler{}
+	}
+	if unmarshaler != nil {
+		return unmarshaler.UnmarshalAddresses(reader)
+	}
+	return nil, fmt.Errorf("unrecognized input suffix: %s", suffix)
+}
+
+func writeDataFile(filename string, addresses []*Address) error {
+	file, closer, err := createDataFile(filename)
+	if closer != nil {
+		defer closer()
+	}
+	if err != nil {
+		return err
+	}
+	return writeData(file, suffixOf(filename), addresses)
+}
+
+func createDataFile(filename string) (io.WriteCloser, func(), error) {
+	file, err := os.Create(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	closer := func() { file.Close() }
+	var writer io.WriteCloser = file
+	var compressor *gzip.Writer
+	if strings.HasSuffix(filename, ".gz") {
+		compressor = gzip.NewWriter(file)
+		closer = func() { compressor.Close(); file.Close() }
+		writer = compressor
+	}
+	return writer, closer, nil
+}
+
+func writeData(writer io.Writer, suffix string,
+	addresses []*Address) error {
+	var marshaler AddressMarshaler
+	switch suffix {
+	// case ".gob":
+	// 	marshaler = GobMarshaler{}
+	// case ".inv":
+	// 	marshaler = InvMarshaler{}
+	case ".jsn", ".json":
+		marshaler = JSONMarshaler{}
+		// case ".txt":
+		// 	marshaler = TxtMarshaler{}
+		// case ".xml":
+		// 	marshaler = XMLMarshaler{}
+	}
+	if marshaler != nil {
+		return marshaler.MarshalAddresses(writer, addresses)
+	}
+	return errors.New("unrecognized output suffix")
+}
+
+func suffixOf(filename string) string {
+	suffix := filepath.Ext(filename)
+	if suffix == ".gz" {
+		suffix = filepath.Ext(filename[:len(filename)-3])
+	}
+	return suffix
 }
