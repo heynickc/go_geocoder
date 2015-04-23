1263c12c25b95892a86fd4af81622542d195cdd9
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2015-04-22 21:07:55 -0400

    Combine geocoder and csv parser

--- csv_marshaller.go
+++ csv_marshaller.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	// "fmt"
 	"io"
 	"strings"
 )
@@ -16,16 +17,6 @@ type Aliases struct {
 type InRecord struct {
 	Address string
 	Zip     string
-}
-
-func ParseInRecord(line []string) (inRecord *InRecord) {
-
-	inRecord = &InRecord{}
-
-	inRecord.Address = strings.ToUpper(line[8])
-	inRecord.Zip = strings.ToUpper(line[9])
-
-	return inRecord
 }
 
 func UnmarshalInRecords(reader *csv.Reader) (inRecords []*InRecord, err error) {
@@ -44,4 +35,42 @@ func UnmarshalInRecords(reader *csv.Reader) (inRecords []*InRecord, err error) {
 	}
 
 	return inRecords, nil
+}
+
+func UnmarshalInRecordsAppendXY(reader *csv.Reader) (inRecords []*InRecord, err error) {
+
+	eof := false
+	for lino := 2; !eof; lino++ {
+		line, err := reader.Read()
+		if err == io.EOF {
+			err = nil
+			eof = true
+			return inRecords, nil
+		} else if err != nil {
+			return nil, err
+		}
+		inRecords = append(inRecords, ParseInRecordAppendXY(line))
+	}
+
+	return inRecords, nil
+}
+
+func ParseInRecord(line []string) (inRecord *InRecord) {
+
+	inRecord = &InRecord{}
+
+	inRecord.Address = strings.ToUpper(line[8])
+	inRecord.Zip = strings.ToUpper(line[9])
+
+	return inRecord
+}
+
+func ParseInRecordAppendXY(line []string) (inRecord *InRecord) {
+
+	inRecord = &InRecord{}
+
+	inRecord.Address = strings.ToUpper(line[8])
+	inRecord.Zip = strings.ToUpper(line[9])
+
+	return inRecord
 }
